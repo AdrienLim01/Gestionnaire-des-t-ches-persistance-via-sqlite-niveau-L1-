4b507a3f8e34434eb90f26d5aba765a5039d7f5c
AdrienLim01 <adrien.erik.lim@gmail.com>
2025-06-03 21:53:41 +0200

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+DB_PATH = "gestionnaire_de_tache.db"
+
+with sqlite3.connect(DB_PATH) as db:
+    db.execute("PRAGMA foreign_keys = ON")
+    
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS utilisateurs(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            prenom TEXT NOT NULL,
+            nom TEXT NOT NULL
+        )
+    """)
+
+
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS taches(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_utilisateur INTEGER,
+            tache_a_realiser TEXT NOT NULL,
+            statut TEXT NOT NULL,
+            FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id)
+        )
+    """)
